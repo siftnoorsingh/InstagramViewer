2e6db9c0fa03817c0479c7b7570f9f8f843dce6d
GaganMelb <k.gaganjot@yahoo.com>
2015-10-07 22:19:42 +1100

    changes

--- app/src/main/java/com/project/gagan/instagram_gagan/Photo.java
+++ app/src/main/java/com/project/gagan/instagram_gagan/Photo.java
@@ -3,5 +3,46 @@ package com.project.gagan.instagram_gagan;
 /**
  * Created by Gagan on 06-Oct-15.
  */
-public class Photo {
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+/*
+ * An extension of ParseObject that makes
+ * it more convenient to access information
+ * about a given Photo
+ */
+
+@ParseClassName("Photo")
+public class Photo extends ParseObject {
+
+    public Photo() {
+        // A default constructor is required.
+    }
+
+    public ParseFile getImage() {
+        return getParseFile("image");
+    }
+
+    public void setImage(ParseFile file) {
+        put("image", file);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser("user");
+    }
+
+    public void setUser(ParseUser user) {
+        put("user", user);
+    }
+
+    public ParseFile getThumbnail() {
+        return getParseFile("thumbnail");
+    }
+
+    public void setThumbnail(ParseFile file) {
+        put("thumbnail", file);
+    }
 }
+
