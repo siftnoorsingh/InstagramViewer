99b0d20797f9c71f86a03f93a715b0cfc1943632
siftnoor <sift.singh@gmail.com>
2015-10-11 22:03:04 -0400

    gallery grid added

--- app/src/main/java/com/project/gagan/instagram_gagan/GalleryTab.java
+++ app/src/main/java/com/project/gagan/instagram_gagan/GalleryTab.java
@@ -1,66 +1,47 @@
 package com.project.gagan.instagram_gagan;
 
-import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.content.ContentResolver;
+import android.widget.Toast;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Sift on 10/10/2015.
  */
 public class GalleryTab extends Fragment{
 
-    protected Cursor mCursor;
-    protected int columnIndex;
-    protected GridView mGridView;
-    protected GalleryAdapter mAdapter;
+    protected GridView gridView;
+    protected GalleryAdapter galleryAdapter;
+
     //private static final int RESULT_OK = 100;
     //private int PICK_IMAGE_REQUEST = 1;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.gallery_tab, container, false);
-        //Intent intent = new Intent();
-        // Show only images, no videos or anything else
-        //intent.setType("image/*");
-        //intent.setAction(Intent.ACTION_GET_CONTENT);
-        // Always show the chooser (if there are multiple options available)
-        //startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 
-        // Get all the images on phone
+        gridView = (GridView) rootView.findViewById(R.id.gridView);
+        galleryAdapter = new GalleryAdapter(rootView.getContext(), R.layout.grid_cell_layout, getData());
+        gridView.setAdapter(galleryAdapter);
 
-        String[] projection = {
-                MediaStore.Images.Thumbnails._ID,
-                MediaStore.Images.Thumbnails.IMAGE_ID
-        };
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(rootView.getContext(), "Image at " + position + " clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        mCursor = getContentResolver().query(
-                MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-                projection,
-                null,
-                null,
-                MediaStore.Images.Thumbnails.IMAGE_ID + " DESC"
-        );
-
-        columnIndex = mCursor.getColumnIndexOrThrow(projection[0]);
-
-        // Get the GridView layout
-        mGridView = (GridView) rootView.findViewById(R.id.gridView);
-        mAdapter = new GalleryAdapter(this);
-        mGridView.setAdapter(mAdapter);
         return rootView;
     }
 
-
+    private ArrayList<Bitmap> getData(){
+        return null;
+    };
 
     //@Override
     /*protected void onActivityResult(int requestCode, int resultCode, Intent data,View rootView) {
